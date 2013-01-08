228fe2e41e0127479447561f758cd6e69f26e6b9
182 student <unknown>
2013-01-08 00:18:52 -0500

    updates

---
+++ createbackup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Make sure that student supplies uniquename as a parameter
+EXPECTED_ARGS=1
+if [ $# -ne $EXPECTED_ARGS ]
+then
+    echo "Usage: `basename $0` your_uniquename"
+    exit 100
+fi
+
+uniquename=$1
+echo $uniquename
+
+# Create the remote repository in ITD space
+
+echo "Attempting to create a homework repository in your ITD space. Enter your umich password when prompted." 
+#ssh $uniquename@login.itd.umich.edu "mkdir eecs182HW;fs setacl eecs182HW $uniquename all -clear; cd eecs182HW; git init --bare" 
+
+firstchar=${uniquename:0:1}
+secondchar=${uniquename:1:1}
+
+git remote add backupserver ssh://$uniquename@login.itd.umich.edu/afs/umich.edu/user/$firstchar/$secondchar/$uniquename/eecs182HW
+
+
+
+
+
---
+++ dobackup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Make sure that student supplies uniquename as a parameter
+EXPECTED_ARGS=1
+if [ $# -ne $EXPECTED_ARGS ]
+then
+    echo "Usage: `basename $0` your_uniquename"
+    exit 100
+fi
+
+uniquename=$1
+echo $uniquename
+
+firstchar=${uniquename:0:1}
+secondchar=${uniquename:1:1}
+
+git remote add backupserver ssh://$uniquename@login.itd.umich.edu/afs/umich.edu/user/$firstchar/$secondchar/$uniquename/eecs182HW
+
+
+
+git add .
+git commit -m "updates"
+git push backupserver master
+
+
+
+
+
+
+
