24f811611df7a67e158e24cbc34e66f138132a26
Atul Prakash <atulweb@gmail.com>
2013-01-19 20:56:43 -0500

    updates

--- pullhwupdates.sh
+++ pullhwupdates.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git commit -m "local update"
-git pull -s merge -X theirs https://github.com/aprakashumich/hw_182w13 master
+git pull -s recursive -X theirs https://github.com/aprakashumich/hw_182w13 master
 git checkout .
 
