57e69b562c7097ba6631aec5e5e3be981c0a62ee
Atul Prakash <atulweb@gmail.com>
2013-02-04 19:01:05 -0500

    allow for plural

--- HW2/format_test.sh
+++ HW2/format_test.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -x verbose
 g++ -g -Wall days.cpp -o days.out
-days.out < in7.txt | sed s/years/year/ > studentout7.txt
-echo "Comparing studentout7.txt and professor output out7.txt"
-diff -s studentout7.txt out7.txt
+days.out < in7.txt | sed s/weeks/week/ | sed s/days/day/ | sed s/years/year/ > studentout7.txt
+cat out7.txt | sed s/weeks/week/ | sed s/days/day/ > out7_s.txt
+echo "comparison ignores plurals and singular words (e.g., day vs. days)"
+echo "Comparing studentout7.txt and professor output out7_s.txt"
+diff -s studentout7.txt out7_s.txt
 
 python stats.py < pin2.txt > student_pout2.txt
 echo "Comparing student_pout2.txt and professor output pout2.txt"
