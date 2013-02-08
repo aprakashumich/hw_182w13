0d88b2d4bb38ecdaeddfb45f17d29cfecd5e1005
Atul Prakash <atulweb@gmail.com>
2013-02-07 19:52:26 -0500

    updates

--- HW2/format_test.sh
+++ HW2/format_test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 g++ -g -Wall days.cpp -o days.out
 echo "Testing: Input file: in1.txt, reference output file: out1.txt"
-days.out < in7.txt | sed s/weeks/week/ | sed s/days/day/ | sed s/years/year/ > studentout7.txt
-cat out7.txt | sed s/weeks/week/ | sed s/days/day/ > out7_s.txt
+days.out < in7.txt | sed s/weeks/week/g | sed s/days/day/g | sed s/years/year/g > studentout7.txt
+cat out7.txt | sed s/weeks/week/g | sed s/days/day/g | sed s/years/year/g  > out7_s.txt
 echo ""
 echo "comparison ignores plurals and singular words (e.g., day vs. days)"
 echo "Comparing studentout7.txt and professor output out7_s.txt"
@@ -10,8 +10,8 @@ diff -s studentout7.txt out7_s.txt
 
 echo ""
 echo "Another test. Input file: in1.txt, reference output file: out1.txt"
-days.out < in1.txt | sed s/weeks/week/ | sed s/days/day/ | sed s/years/year/ > studentout1.txt
-cat out1.txt | sed s/weeks/week/ | sed s/days/day/ > out1_s.txt
+days.out < in1.txt | sed s/weeks/week/g | sed s/days/day/g | sed s/years/year/g > studentout1.txt
+cat out1.txt | sed s/weeks/week/g | sed s/days/day/g | sed s/years/year/g  > out1_s.txt
 echo ""
 echo "comparison ignores plurals and singular words (e.g., day vs. days)"
 echo "Comparing studentout1.txt and professor output out1_s.txt"
